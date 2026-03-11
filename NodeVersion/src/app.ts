dfc951b42d110ecb05bc360749c37011bdfe2c94
Vinicius Delmo <vinicius.delmo@gmail.com>
2026-03-11 17:34:28 -0300

    feat: add prisma client and error middleware

--- NodeVersion/src/app.ts
+++ NodeVersion/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { json } from 'body-parser';
+import { errorHandler } from './middlewares/error.middleware';
 
 dotenv.config();
 
@@ -8,5 +9,7 @@ const app = express();
 app.use(json());
 
 app.get('/health', (req, res) => res.json({ ok: true }))
+
+app.use(errorHandler)
 
 export default app;
